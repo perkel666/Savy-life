d005061542a1fd3605369637edfb0eff643305e4
perkel666 <ciotazemnie@gmail.com>
2016-03-08 12:57:23 +0100

    Changes MainMenu():
    - added values
    - refactored a bit code
    - refactored show_menu() to show buttons in orderly
      fassion

--- main.py
+++ main.py
@@ -19,21 +19,58 @@ def load_image(name):
 class MainMenu():
     def __init__(self):
         self.visible = True
+        self.position_x = 50
+        self.position_y = 50
+
+        self.continue_button = ContinueButton()
+        self.newgame_button = NewGameButton()
+        self.save_button = SaveButton()
+        self.load_button = LoadButton()
+        self.option_button = OptionsButton()
+        self.quit_button = QuitButton()
 
     def show_menu(self):
         if self.visible is True:
+
+            # MENU LOGIC
+            # 1. Creating list of menu options
+
+            menu_options = [
+                self.continue_button,
+                self.newgame_button,
+                self.save_button,
+                self.load_button,
+                self.option_button,
+                self.quit_button
+            ]
+            # 2. Creating list of visible menu options according to self.visible in button
+
+            menu_options_active = []
+
+            for menu_option in menu_options:
+                if menu_option.visible is True:
+                    menu_options_active.append(menu_option)
+
+            # 3. Creating Buttons sprite group from menu_options_active
+
+            menu_buttons = pygame.sprite.Group()
+            for button in menu_options_active:
+                button.rect.y = (self.position_y - 50) + (75 * button.order)
+                button.rect.x = self.position_x + 17
+                menu_buttons.add(button)
+
+            # UPDATE GRAPHIC
             main_menu_transparency = load_image('menu_transparency.png')
             main_menu_background = MainMenuBackground()
             middle_sprites = pygame.sprite.Group(main_menu_background)
-            new_game_button = NewGameButton()
-            top_sprites = pygame.sprite.Group(new_game_button)
 
+            menu_buttons.update()
             middle_sprites.update()
-            top_sprites.update()
 
+            # DISPLAY
             screen.blit(main_menu_transparency[0], (0, 0))
             middle_sprites.draw(screen)
-            top_sprites.draw(screen)
+            menu_buttons.draw(screen)
         else:
             pass
 
@@ -148,8 +185,8 @@ class Game(object):
         # GAME INITIALIZATION
         clock = pygame.time.Clock()
         main_menu = MainMenu()
-        background_image = GameBackground()
 
+        background_image = GameBackground()
         background = pygame.Surface(screen.get_size())
         background = background.convert()
         background.fill((200, 200, 200))
