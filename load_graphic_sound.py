d2a0a039da8c55ede1e6ee7cb1ba23cfb397a333
perkel666 <ciotazemnie@gmail.com>
2016-03-10 23:23:04 +0100

    Changes MainMenu():
    - added values
    - refactored code
    - refactored show_menu() to show buttons in orderly
      fassion
    - cleaned up junk
    - added option to change MainMenu position
    - added option to set buttons position relative to background image
    - added mouse hover button graphic change
    - added game shitdown via button quit
    - split Main Menu from main.py into separate files.

---
+++ load_graphic_sound.py
@@ -0,0 +1,16 @@
+__author__ = 'Perkel'
+
+import os
+import pygame
+
+
+def load_image(name):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    image = image.convert_alpha()
+
+    return image, image.get_rect()
