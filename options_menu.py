8c1c1183c3af7224ee9282cec0b2ec3887a5ab82
perkel666 <ciotazemnie@gmail.com>
2016-03-12 11:58:09 +0100

    - added body image to options menu

--- options_menu.py
+++ options_menu.py
@@ -14,12 +14,10 @@ class OptionsMenu():
         self.options_menu_background = OptionsMenuBackground()
         self.options_menu_background.rect.x = 50
         self.options_menu_background.rect.y = 50
-
         # close button and position
         self.close_button = CloseButton()
         self.close_button.rect.x = self.options_menu_background.rect.x + 700
         self.close_button.rect.y = self.options_menu_background.rect.y + 50
-
         # Buttons
         self.buttons_position_x = self.options_menu_background.rect.x + 30
         self.buttons_position_y = self.options_menu_background.rect.y + 40
@@ -27,7 +25,6 @@ class OptionsMenu():
         self.button_sound = ButtonSound()
         self.button_back = ButtonBack()
         self.button_display = ButtonDisplay()
-
         # List of buttons
         self.buttons_list = [
             self.button_game,
@@ -35,6 +32,10 @@ class OptionsMenu():
             self.button_sound,
             self.button_back
         ]
+        # Buttons body
+        self.options_body_main_background = OptionsBody()
+        self.options_body_main_background.rect.x = self.options_menu_background.rect.x + 275
+        self.options_body_main_background.rect.y = self.options_menu_background.rect.y + 40
 
     def show_menu(self, screen, game):
         if game.options_menu_visible is True:
@@ -66,12 +67,22 @@ class OptionsMenu():
             for button in self.buttons_list:
                 sprite_hover(button)
                 buttons_layer.add(button)
+
+            # display button body
+
+            options_body_main = pygame.sprite.Group()
+            for button in self.buttons_list:
+                if button.selected is True:
+                    options_body_main.add(self.options_body_main_background)
+
             #
             # INPUT
             #
 
+            # check if mouse hovers over button and if clicked with left mouse button DO SOMETHING
             if game.input_control is "options_menu":
                 for event in pygame.event.get():
+
                     # - CLOSE BUTTON (RIGHT CORNER)
                     if event.type == pygame.MOUSEBUTTONUP and \
                             event.button == 1 and \
@@ -80,47 +91,56 @@ class OptionsMenu():
                         game.options_menu_visible = False
                         game.input_control = "main_menu"
                         print "show main menu"
+
                     # GAME BUTTON
                     if event.type == pygame.MOUSEBUTTONUP and \
                             event.button == 1 and \
                             self.button_game.rect.collidepoint(pygame.mouse.get_pos()):
-                        ###
-                        ###DO SOMETHING
-                        ###
+                        self.button_game.selected = True
+                        self.button_display.selected = False
+                        self.button_sound.selected = False
                         print "button_game"
+
                     # DISPLAY BUTTON
                     if event.type == pygame.MOUSEBUTTONUP and \
                             event.button == 1 and \
                             self.button_display.rect.collidepoint(pygame.mouse.get_pos()):
-                        ###
-                        ###DO SOMETHING
-                        ###
+                        self.button_game.selected = False
+                        self.button_display.selected = True
+                        self.button_sound.selected = False
                         print "button_display"
+
                     # SOUND BUTTON
                     if event.type == pygame.MOUSEBUTTONUP and \
                             event.button == 1 and \
                             self.button_sound.rect.collidepoint(pygame.mouse.get_pos()):
-                        ###
-                        ###DO SOMETHING
-                        ###
+                        self.button_game.selected = False
+                        self.button_display.selected = False
+                        self.button_sound.selected = True
                         print "button_sound"
+
                     # BACK BUTTON
                     if event.type == pygame.MOUSEBUTTONUP and \
                             event.button == 1 and \
                             self.button_back.rect.collidepoint(pygame.mouse.get_pos()):
                         game.main_menu_visible = True
                         game.options_menu_visible = False
+                        self.button_game.selected = False
+                        self.button_display.selected = False
+                        self.button_sound.selected = False
                         game.input_control = "main_menu"
                         print "button_back"
 
             # UPDATE GRAPHIC
             self.transparency.update()
             background.update()
+            options_body_main.update()
             buttons_layer.update()
             buttons_sprite_layer.update()
             # DISPLAY
             self.transparency.draw(screen)
             background.draw(screen)
+            options_body_main.draw(screen)
             buttons_layer.draw(screen)
             buttons_sprite_layer.draw(screen)
 
@@ -148,6 +168,7 @@ class ButtonDisplay(pygame.sprite.Sprite):
         self.image, self.rect = load_image('button_display.png')
         self.image_no_hover = self.image
         self.image_hover, self.rect = load_image('button_display_hover.png')
+        self.selected = False
 
 
 class ButtonSound(pygame.sprite.Sprite):
@@ -156,6 +177,7 @@ class ButtonSound(pygame.sprite.Sprite):
         self.image, self.rect = load_image('button_sound.png')
         self.image_no_hover = self.image
         self.image_hover, self.rect = load_image('button_sound_hover.png')
+        self.selected = False
 
 
 class ButtonGame(pygame.sprite.Sprite):
@@ -164,6 +186,7 @@ class ButtonGame(pygame.sprite.Sprite):
         self.image, self.rect = load_image('button_game.png')
         self.image_no_hover = self.image
         self.image_hover, self.rect = load_image('button_game_hover.png')
+        self.selected = False
 
 
 class ButtonBack(pygame.sprite.Sprite):
@@ -172,3 +195,11 @@ class ButtonBack(pygame.sprite.Sprite):
         self.image, self.rect = load_image('button_back.png')
         self.image_no_hover = self.image
         self.image_hover, self.rect = load_image('button_back_hover.png')
+        self.selected = False
+
+
+class OptionsBody(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('options_menu_body.png')
+        self.visible = False
