161432e100d2c3aff746bfcf1f869a0f75cf3ea0
perkel666 <ciotazemnie@gmail.com>
2016-03-13 11:31:42 +0100

    - reworked :   load_image()   load_sprite()
     for now they are in test phase as load_image2() and load_spriter()
    
     Descr:
      after test phase load_image() will be internal function of load_sprite(), load_sprite() will handle automatically all cases of sprite, sprite having hover image, sprite having hover and pressed image
      also sprite will have now get_state()function which will be able to set self.image according to what is happening on screen. In case of pressed it will get collision and change image for image_pressed and so on.

--- load_graphic_sound.py
+++ load_graphic_sound.py
@@ -25,6 +25,7 @@ def load_sprite(name):
             pygame.sprite.Sprite.__init__(self)
             self.image, self.rect = load_image(name)
             self.visible = True
+
     sprite = CreateSprite()
     return sprite
 
@@ -34,3 +35,83 @@ def find(name):
         if name in files:
             print os.path.join(root, name)
             return os.path.join(root, name)
+
+
+def load_image_2(name, rect=None):
+    file_path = find(name)
+    """
+    fullname = os.path.join('data', name)
+    """
+    if rect is not None:
+        try:
+            image = pygame.image.load(file_path)
+        except pygame.error, message:
+            print 'Cannot load image:', name
+            raise SystemExit, message
+        image = image.convert_alpha()
+        return image, image.get_rect()
+
+    else:
+        try:
+            image = pygame.image.load(file_path)
+        except pygame.error, message:
+            print 'Cannot load image:', name
+            raise SystemExit, message
+        image = image.convert_alpha()
+        return image
+
+
+def load_spriter(name, hover=None, pressed=None):
+    class CreateSprite(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image, self.rect = load_image(name)
+            self.visible = True
+
+            # PRESSED AND HOVER NEEDS TO BE SAME SIZE
+            # TO WORK PROPERLY IN GAME
+            if hover is not None and hover is True:
+                self.image_no_hover = self.image
+                path, ext = os.path.splitext(name)
+                self.image_hover = load_image_2(find(path+'_hover.png'))
+            elif hover is not None and hover is not True:
+                print "hover shound be True got something else instead for ", name
+            if pressed is not None and pressed is True:
+                path, ext = os.path.splitext(name)
+                self.image_pressed = load_image_2(find(path+'_pressed.png'))
+            elif pressed is not None and pressed is not True:
+                print "pressed shoud be True got something else instead for ", name
+
+        def get_state(self):
+
+            # if there is pressed and hover image
+            if self.image_hover is not None and \
+                    self.image_pressed is not None:
+
+                if self.rect.collidepoint(pygame.mouse.get_pos()):
+                    for event in pygame.event.get():
+                        # pressed
+                        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                            sprite.image = sprite.image_pressed
+                        # not pressed
+                        else:
+                            sprite.image = sprite.image_hover
+                else:
+                    if sprite.image != sprite.image_no_hover:
+                        sprite.image = sprite.image_no_hover
+                    else:
+                        pass
+            # if there is hover but there isn't pressed
+            elif self.image_hover is not None and self.image_pressed is None:
+                if sprite.rect.collidepoint(pygame.mouse.get_pos()):
+                    sprite.image = sprite.image_hover
+                else:
+                    if sprite.image != sprite.image_no_hover:
+                        sprite.image = sprite.image_no_hover
+            else:
+                pass
+
+
+
+    sprite = CreateSprite()
+    return sprite
--- sprite_effects.py
+++ sprite_effects.py
@@ -5,7 +5,7 @@ import pygame
 
 def sprite_hover(sprite):
     if sprite.rect.collidepoint(pygame.mouse.get_pos()):
-                    sprite.image = sprite.image_hover
+        sprite.image = sprite.image_hover
     else:
         if sprite.image != sprite.image_no_hover:
             sprite.image = sprite.image_no_hover
